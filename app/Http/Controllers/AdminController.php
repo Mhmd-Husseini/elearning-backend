c65ac2a88ae24d2cc70c4dd6d258e1fdb8f62dc5
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-08-11 10:25:25 +0300

    Server  <laravel> download composer

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\User_type;
+use Illuminate\Http\Request;
+use Exception;
+
+class AdminController extends Controller
+{
+    
+}
